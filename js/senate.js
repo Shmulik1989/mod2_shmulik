382e4538f3a1fe16b7da6f52c870dcd939cecd8b
Retina <retina@Retinas-MacBook-Pro.local>
2017-12-01 14:07:14 +0100

    index clean

---
+++ js/senate.js
@@ -0,0 +1,45 @@
+function convertParty(party) {
+    if (party == "R") {
+        return "Republicans"
+    } else if (party == "D") {
+        return "Democrats"
+
+    } else {
+        return "Indipendent"
+    }
+
+
+}
+
+function checkMidName(middleName) {
+    if (middleName === null) {
+        return "";
+    } else {
+        return middleName;
+
+
+    }
+
+
+}
+
+
+
+var data = $.getJSON("data/pro_congress_113_senate.json", function(data) {
+    console.log(data);
+    var members = data.results[0].members
+
+
+    for (var i = 0; i < members.length; i++) {
+        var table = "<tr>"
+        table += "<td>" + '<a href="' + members[i].url + '">' + members[i].first_name + " " + checkMidName(members[i].middle_name) + " " + members[i].last_name + "</td>"
+        table += "<td>" + members[i].state + "</td>"
+        table += "<td>" + convertParty(members[i].party) + "</td>"
+        table += "<td>" + members[i].seniority + "</td>"
+        table += "<td>" + members[i].votes_with_party_pct + "%</td>";
+        table += "</tr>"
+
+        $("#member-table").append(table);
+    }
+
+})
